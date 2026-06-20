c2eb3c8d9531132960368cc5ac0a026cd162990d
mnli001 <mmanlai269@gmail.com>
2026-06-20 18:22:19 +0800

    app js suuri holbov

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// app.js
+const INSTRUMENTS = {
+    'XAUUSD': { name: 'XAUUSD', contractSize: 100, defaultPrice: 2500.00, pipSize: 0.10 },
+    'EURUSD': { name: 'EURUSD', contractSize: 100000, defaultPrice: 1.0850, pipSize: 0.0001 }
+};
+let tradesArray = [];
